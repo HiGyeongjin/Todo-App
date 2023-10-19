7c9efe9d8bd92f98a34feb6dfb045adf2dabd329
Gyeongjin Lee <asdfg031897@gmail.com>
2023-10-19 16:31:31 -0700

    added listview but not fixed bug yet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import InsertForm from "./components/insertForm";
+import ListView from "./components/listView";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  const handleInsert = (value) => {
+    setTodoList((current) => {
+      //여기서 current는 현재값을 불러와서 맨뒤에 새로 입력된 값을
+      //삽입해준다는 의미임.현재값을 전달받는 함수를 생성중임.
+      const newTodoList = [...current]; //array나 object를 갖는 state일 경우, 새로운 변수를
+      //생성한 뒤 값을 복사해와야 함.
+      newTodoList.push({
+        key: new Date().getTime(), // JSX에서 Array의 값을 표현할 때 각 요소를 구분하기 위한 값
+        value: value, // onInsert로부터 전달받은 값,
+        isCompleted: false, // 완료 처리를 위한 flag
+      });
+      return newTodoList;
+    });
+  };
   return (
     <div className="App">
-      <InsertForm
-        onInsert={(value) => {
-          console.log(value);
-        }}
-      />
+      <ListView todoList={todoList} />
+      <InsertForm onInsert={handleInsert} />
     </div>
   );
 }
--- src/components/insertForm.js
+++ src/components/insertForm.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { useState } from "react";
 
 const InsertForm = ({ onInsert }) => {
   const [inputValue, setInputValue] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (typeof onInsert === "function") {
-      onInsert(inputValue); //props로부터 전달받은 onInsert함수를 호출. 그때 전달받는 값은
-      //inputValue로 전달받음.
-    }
-    setInputValue(""); //이거는 엔터치고 나서 submit하고 input값을 초기화해줌.
-  };
+  const handleSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+      if (typeof onInsert === "function" && inputValue) {
+        onInsert(inputValue); //props로부터 전달받은 onInsert함수를 호출. 그때 전달받는 값은
+        //inputValue로 전달받음.
+      }
+      setInputValue(""); //이거는 엔터치고 나서 submit하고 input값을 초기화해줌.)
+    },
+    [onInsert, inputValue]
+  );
   return (
     <form onSubmit={handleSubmit}>
       <input
